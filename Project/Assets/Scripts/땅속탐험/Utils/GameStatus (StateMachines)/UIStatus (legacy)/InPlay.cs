639c93330714f70c55b4ca0c9106fb5014ed276e
mati-as <gongminseok505@gmail.com>
2023-10-10 16:39:28 +0900

    땅속탐험 [feat] UI에셋 및 tween 임포트

--- Project/Assets/Scripts/땅속탐험/Utils/GameStatus (StateMachines)/UIStatus (legacy)/InPlay.cs
+++ Project/Assets/Scripts/땅속탐험/Utils/GameStatus (StateMachines)/UIStatus (legacy)/InPlay.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using 땅속탐험.Utils.GameStatus.UIStatus;
 
-public class InPlay : MonoBehaviour
+public class  InPlay : MonoBehaviour
 {
     public IUIState.UIStateList UIState => IUIState.UIStateList.InPlay;
+    
     public void Enter()
     {
         
