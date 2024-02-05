563633227940dedd465ee547efebf7806961d90f
Jeongseongwon <j001656100@gmail.com>
2024-02-05 12:52:28 +0900

    ver 0.0.1
    * 신형 라이다 연동 dll 파일 수정

--- Project/Assets/RplidarTest/RplidarTest_Ray.cs
+++ Project/Assets/RplidarTest/RplidarTest_Ray.cs
@@ -30,7 +30,6 @@ public class RplidarTest_Ray : MonoBehaviour
 
     //====1012
     public bool Test_check = false;
-    double number = 0f;
 
     public GameObject Guideline;
     public GameObject TESTUI;
@@ -58,7 +57,6 @@ public class RplidarTest_Ray : MonoBehaviour
     public float Sensor_rotation = 0;
 
     //1121
-
     private GameObject UI_Canvas;
     private Camera UI_Camera;
 
@@ -66,7 +64,21 @@ public class RplidarTest_Ray : MonoBehaviour
     private float x;
     private float y;
 
+    //0126
     private bool Check_1 = false;
+    private List<float> pos_x = new List<float>();
+    private List<float> pos_y = new List<float>();
+
+
+    private float min_pos_x;
+    private float min_pos_y;
+    private float max_pos_x;
+    private float max_pos_y;
+
+    private float avg_pos_x;
+    private float avg_pos_y;
+
+    private float Check_box = 100;
 
     private void Awake()
     {
@@ -95,12 +107,24 @@ public class RplidarTest_Ray : MonoBehaviour
         UI_Canvas = Manager_Sensor.instance.Get_UIcanvas();
         UI_Camera = Manager_Sensor.instance.Get_UIcamera();
 
+
         //guide라인이랑 동기화 기능
         min_x = Guideline.GetComponent<RectTransform>().anchoredPosition.x - (Guideline.GetComponent<RectTransform>().rect.width) / 2;
         min_y = Guideline.GetComponent<RectTransform>().anchoredPosition.y - (Guideline.GetComponent<RectTransform>().rect.height) / 2;
         max_x = Guideline.GetComponent<RectTransform>().anchoredPosition.x + (Guideline.GetComponent<RectTransform>().rect.width) / 2;
         max_y = Guideline.GetComponent<RectTransform>().anchoredPosition.y + (Guideline.GetComponent<RectTransform>().rect.height) / 2;
 
+
+        pos_x.Clear();
+        pos_y.Clear();
+
+        avg_pos_x = 0;
+        avg_pos_y = 0;
+
+        min_pos_x = 0;
+        min_pos_y = 0;
+        max_pos_x = 0;
+        max_pos_y = 0;
     }
 
 
@@ -126,6 +150,7 @@ public class RplidarTest_Ray : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Test_check = !Test_check;
+
         }
 
         if (Input.GetKeyDown(KeyCode.U))
@@ -146,21 +171,21 @@ public class RplidarTest_Ray : MonoBehaviour
     void FixedUpdate()
     {
 
+        //복잡도 개선이 가능한지 확인 필요함
         if (m_datachanged)
         {
             for (int i = 0; i < 720; i++)
             {
                 //센서 데이터 data[i].theta, distant
                 //1. 화면과 센서를 일치화 시키기 위해서 theta를 마이너스 곱해줌, 추가로 회전 시켜주기 위해 Sensor_rotation 추가했고 위에서 아래 방향으로 내려다 보는것 기준으 90도 입력하면 댐
-                x = 0.74f * Mathf.Cos((-data[i].theta + Sensor_rotation) * Mathf.Deg2Rad) * (data[i].distant * 1.05f);
-                y = 540 + 0.74f * Mathf.Sin((-data[i].theta + Sensor_rotation) * Mathf.Deg2Rad) * (data[i].distant * 1.05f);
+                x = 0.74f * Mathf.Cos((-data[i].theta + Sensor_rotation) * Mathf.Deg2Rad) * data[i].distant;
+                y = 540 + 0.74f * Mathf.Sin((-data[i].theta + Sensor_rotation) * Mathf.Deg2Rad) * data[i].distant;
 
                 if (i % 4 == 0)
                 {
-
-
                     if (x != 0 || y != 0)
                     {
+                        //가이드 안에 들어가는지 확인
                         if (min_x < x && x < max_x)
                         {
                             if (min_y < y && y < max_y)
@@ -168,16 +193,13 @@ public class RplidarTest_Ray : MonoBehaviour
                                 if (Test_check)
                                 {
                                     //데모용, 마우스
-                                    TESTUI.SetActive(false);
-                                    Instant_Ball(x, y);
+                                    Instant_Mouse(x, y);
                                 }
                                 else
                                 {
                                     //개발자용, 공
-                                    TESTUI.SetActive(true);
-                                    Instant_Mouse(x, y);
+                                    Instant_Ball(x, y);
                                 }
-
 
                             }
                         }
